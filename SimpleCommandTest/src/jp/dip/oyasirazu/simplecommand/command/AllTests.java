201ab3ebc202256afefd507878c38496e14b5c09
mikoto2000 <mikoto2000@gmail.com>
2013-01-03 02:20:15 +0900

    Added test suite class.

---
+++ SimpleCommandTest/src/jp/dip/oyasirazu/simplecommand/command/AllTests.java
@@ -0,0 +1,11 @@
+package jp.dip.oyasirazu.simplecommand.command;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CommandTest.class})
+public class AllTests {
+}
